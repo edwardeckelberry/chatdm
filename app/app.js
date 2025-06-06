71a22dbd44a45fac832c82f71260d37627ae0b17
ed <edward.eckelberry@gmail.com>
2025-06-05 21:51:42 -0700

    now it actually works

--- app/app.js
+++ app/app.js
@@ -1,27 +1,21 @@
-const socket = io('ws://localhost:3500');
+const socket = io('ws://localhost:3500')
 
-//e = event
-function sendMessage(e){
-    //so the form doesn't refresh the page
+function sendMessage(e) {
     e.preventDefault()
     const input = document.querySelector('input')
-    if (input.value){
-        //send the message to the server
-        socket.send(input.value)
+    if (input.value) {
+        socket.emit('message', input.value)
         input.value = ""
     }
-    //go back to the input so the user can type again
     input.focus()
 }
 
-//once the connection is open, send a message
-document.querySelector('form').addEventListener('submit', sendMessage)
+document.querySelector('form')
+    .addEventListener('submit', sendMessage)
 
-//listen for messages from the server
+// Listen for messages 
 socket.on("message", (data) => {
-    //create a new list item
     const li = document.createElement('li')
-    //retrive the message from the server
     li.textContent = data
     document.querySelector('ul').appendChild(li)
 })
--- server/index.js
+++ server/index.js
@@ -1,22 +1,20 @@
-import { create } from "domain"
 import { createServer } from "http"
 import { Server } from "socket.io"
-//create a socket.io server in port 3000
+
 const httpServer = createServer()
+
 const io = new Server(httpServer, {
     cors: {
-        //allow all origins in development, but only the origin of the app in production
-        // to have all origins, set origin: "*"
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
 })
 
-//once there is a connection, listen for a message
 io.on('connection', socket => {
-    console.log('User ' + socket.id + ' connected') 
-    socket.on('message', message => {
+    console.log(`User ${socket.id} connected`)
+
+    socket.on('message', data => {
         console.log(data)
-        io.emit('message', '${socket.id.substring(0,5): ${data}')
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
     })
 })
 
