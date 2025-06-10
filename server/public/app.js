69db19e4c85c4596a6f1e438d9184a5b3abced8c
ed <edward.eckelberry@gmail.com>
2025-06-10 10:19:17 -0700

    adding activity detection

--- server/public/app.js
+++ server/public/app.js
@@ -1,13 +1,14 @@
 const socket = io('ws://localhost:3500')
 
+const activity = document.querySelector('.activity')
+const msgInput = document.querySelector('input')
 function sendMessage(e) {
     e.preventDefault()
-    const input = document.querySelector('input')
-    if (input.value) {
-        socket.emit('message', input.value)
-        input.value = ""
+    if (msgInput.value) {
+        socket.emit('message', msgInput.value)
+        msgInput.value = ""
     }
-    input.focus()
+    msgInput.focus()
 }
 
 document.querySelector('form')
@@ -20,7 +21,6 @@ socket.on("message", (data) => {
     document.querySelector('ul').appendChild(li)
 })
 
-const name = document.querySelector(".name")
-function changeColor(color) {
-    name.style.color = "palevioletred"
-}
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', socket.id.substring(0, 5))
+})
