0306fe2be4711b4f076f39fe9443f881e5ffdac5
ed <edward.eckelberry@gmail.com>
2025-06-10 10:30:49 -0700

    user can see another typing

--- server/public/app.js
+++ server/public/app.js
@@ -16,6 +16,7 @@ document.querySelector('form')
 
 // Listen for messages 
 socket.on("message", (data) => {
+    activity.textContent = ''
     const li = document.createElement('li')
     li.textContent = data
     document.querySelector('ul').appendChild(li)
@@ -23,4 +24,8 @@ socket.on("message", (data) => {
 
 msgInput.addEventListener('keypress', () => {
     socket.emit('activity', socket.id.substring(0, 5))
+})
+
+socket.on('activity', (name) => {
+    activity.textContent = '${name} is typing...'
 })
