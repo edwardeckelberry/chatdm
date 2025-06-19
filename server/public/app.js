6102a4034b19f424f8741583b4fb56f54d22a8e7
ed <edward.eckelberry@gmail.com>
2025-06-19 16:02:52 -0700

    updated app to have usernames

--- server/public/app.js
+++ server/public/app.js
@@ -10,15 +10,35 @@ const chatDisplay = document.querySelector('.chat-display')
 
 function sendMessage(e) {
     e.preventDefault()
-    if (msgInput.value) {
-        socket.emit('message', msgInput.value)
+    if (chatRoom.value && nameInput.value && msgInput.value) {
+        socket.emit('message', {
+            "name": nameInput.value,
+            "text": msgInput.value
+        })
         msgInput.value = ""
     }
     msgInput.focus()
 }
 
-document.querySelector('form')
+function enterRoom(e){
+    e.preventDefault()
+    if (chatRoom.value && nameInput.value) {
+        socket.emit('enterRoom', {
+            name: nameInput.value,
+            room: chatRoom.value
+        }) 
+    }
+}
+
+document.querySelector('.form-msg')
     .addEventListener('submit', sendMessage)
+
+document.querySelector('.form-join')
+    .addEventListener('submit', enterRoom)
+
+msgInput.addEventListener('keypress', () => {
+    socket.emit('activity', nameInput.value)
+})
 
 // Listen for messages 
 socket.on("message", (data) => {
@@ -26,10 +46,6 @@ socket.on("message", (data) => {
     const li = document.createElement('li')
     li.textContent = data
     document.querySelector('ul').appendChild(li)
-})
-
-msgInput.addEventListener('keypress', () => {
-    socket.emit('activity', socket.id.substring(0, 5))
 })
 
 let activityTimer
