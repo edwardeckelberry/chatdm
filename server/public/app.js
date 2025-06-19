cae39a2a818eb5b0dcd3934b231bbf60e770f28b
ed <edward.eckelberry@gmail.com>
2025-06-19 15:54:54 -0700

    added more UI elements

--- server/public/app.js
+++ server/public/app.js
@@ -1,9 +1,13 @@
 const socket = io('ws://localhost:3500')
 
-const activity = document.querySelector('.activity')
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
 const chatRoom = document.querySelector('#room')
+const activity = document.querySelector('.activity')
+const userList = document.querySelector('.user-list')
+const roomList = document.querySelector('.room-list')
+const chatDisplay = document.querySelector('.chat-display')
+
 function sendMessage(e) {
     e.preventDefault()
     if (msgInput.value) {
